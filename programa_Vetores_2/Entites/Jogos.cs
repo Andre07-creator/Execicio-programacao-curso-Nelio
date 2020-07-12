68bff10d5f587fa7b2778fa9f306685a18f48c49
Andre <aramaldes876@gmail.com>
2020-07-11 21:06:12 -0300

    Execicio sobre vetores V1.0

---
+++ programa_Vetores_2/Entites/Jogos.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Globalization;
+
+
+namespace programa_Vetores_2.Entites
+{
+    class Jogos
+    {
+        public string NomeJ { get; set; }
+        public double PrecoJ { get; set; }
+
+        public Jogos()
+        {
+        }
+
+        public Jogos(string nomeJ, double precoJ)
+        {
+            NomeJ = nomeJ;
+            PrecoJ = precoJ;
+        }
+
+        public override string ToString()
+        {
+            return "Jogo:" + NomeJ + ", Preço:" + PrecoJ.ToString("F2" , CultureInfo.InvariantCulture);
+        }
+    }
+}
---
+++ programa_Vetores_2/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using programa_Vetores_2.Entites;
+using System.Globalization;
+
+namespace programa_Vetores_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Quantos jogos deseja cadastrar:");
+            int n = int.Parse(Console.ReadLine());
+
+            Jogos[] vect = new Jogos[n];
+
+            for (int i = 0; i < n; i++)
+            {
+                Console.Write("Digite o nome do jogo:");
+                string nome = Console.ReadLine();
+                Console.Write("Informe o preço do jogo:");
+                double preco = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+                vect[i] = new Jogos{ NomeJ = nome, PrecoJ = preco };
+            }
+            Console.WriteLine();
+            Console.WriteLine("Jogos cadastrados com sucesso no sistema!!!");
+            Console.WriteLine();
+            for (int i = 0; i < n; i++)
+            {
+                Console.WriteLine(vect[i]);
+            }
+        }
+    }
+}
